c37a08b429613506bb0395e296d2af01e8d3b263
Matthew Casperson <matthewcasperson@gmail.com>
2016-07-19 18:09:18 +1000

    initial import

---
+++ spec/iridium-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IridiumSnippetsView from '../lib/iridium-snippets-view';
+
+describe('IridiumSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
